8243ab549218e90737912f4637bf2f4a5fe4370c
Daniil <ladeishikoff.d@yandex.ru>
2021-12-06 21:01:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let upButton = document.querySelector('.up-button')
-// Проверяем условие скролла, если больше 200 применяем новый класс, если меньше 200 то удаляем класс
-window.onscroll = function (){
-    if (window.pageYOffset > 200) {
-        upButton.classList.add('show')
-    } else {upButton.classList.remove('show')};   
-}
+let upButton = document.querySelector('.up-button');
 
-// обработчик клика
-upButton.oneclick = function (){
-    window.scrollTo(0,0);
+window.onscroll = function () {
+  if (window.pageYOffset > 200) {
+    upButton.classList.add('shown');
+  } else {
+    upButton.classList.remove('shown');
+  }
 
+};
+
+upButton.onclick = function () {
+  window.scrollTo(0, 0);
 };
